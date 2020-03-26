d0d92898b8275d6d439beae455ca7b45d2eb368e
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2020-03-26 19:16:58 +0000

    Sending and Receiving Data Using TCP

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -3,18 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace GameServer
 {
     class Program
     {
+        private static bool isRunning = false;
+
         static void Main(string[] args)
         {
             Console.Title = "Matties Server";
+            isRunning = true;
+
+            Thread mainThread = new Thread(new ThreadStart(MainThread));
+            mainThread.Start();
 
             Server.Start(50, 26950);
+        }
 
-            Console.ReadKey();
+        private static void MainThread()
+        {
+            Console.WriteLine($"Main thread started. Running at {Constants.TICKS_PER_SEC} ticks per second.");
+            DateTime _nextLoop = DateTime.Now;
+
+            while (isRunning)
+            {
+                while (_nextLoop < DateTime.Now)
+                {
+                    GameLogic.Update();
+
+                    _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
+
+                    // Reduces the cpu cost when thread is idle
+                    if (_nextLoop > DateTime.Now)
+                    {
+                        Thread.Sleep(_nextLoop - DateTime.Now);
+                    }
+                }
+            }
         }
     }
 }
